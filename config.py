60099b3c0d3f411d590bd5ac65c28be46157bfb7
Ritom <ritomsonowal@gmail.com>
2019-06-01 21:32:12 +0530

    Add config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# consists of GitHub Authentication values
+# this file is not tracked by git
+# DO NOT REMOVE THIS FILE FROM .gitignore due to security reasons
+
+# enter your GitHub credentials
+username = ''
+password = ''
